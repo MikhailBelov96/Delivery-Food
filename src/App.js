8a79f0681af7bdf6204f2678805f9ef7927c09d5
MishaBelove96 <mishabelove72@gmail.com>
2023-05-19 19:23:56 +0400

    started to create react router

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import Skeleton from './components/CardBlock/Skeleton';
-import CardBlock from './components/CardBlock';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
 import './scss/app.scss';
 
 function App() {
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    fetch('https://6467661d2ea3cae8dc2d9f13.mockapi.io/items')
-      .then((res) => res.json())
-      .then((arr) => {
-        setItems(arr);
-        setIsLoading(false);
-      });
-  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-              : items.map((obj) => <CardBlock key={obj.id} {...obj} />)}
-          </div>
+          <Home />
         </div>
       </div>
     </div>
---
+++ src/components/NotFoundBlock/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFoundBlock = () => {
+  return (
+    <div>
+      <h1>
+        <span>😐</span>
+        <br />
+        Ничего не найдено
+      </h1>
+    </div>
+  );
+};
+export default NotFoundBlock;
