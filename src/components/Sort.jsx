9c5b6d0bdc1a44756661d7416ac20aa4dba89e7f
MishaBelove96 <mishabelove72@gmail.com>
2023-05-19 13:10:23 +0400

    created toggling and rendered sort list

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,14 @@
+import React from 'react';
+
 function Sort() {
+  const [open, setOpen] = React.useState(false);
+  const [selected, setSelected] = React.useState(0);
+  const sortList = ['популярности', 'цене', 'алфавиту'];
+  const sortName = sortList[selected];
+  const onClickSorted = (i) => {
+    setSelected(i);
+    setOpen(false);
+  };
   return (
     <div className="sort">
       <div className="sort__label">
@@ -14,15 +24,22 @@ function Sort() {
           />
         </svg>
         <b>Сортировка по:</b>
-        <span>популярности</span>
+        <span onClick={() => setOpen(!open)}>{sortName}</span>
       </div>
-      <div className="sort__popup">
-        <ul>
-          <li className="active">популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {open && (
+        <div className="sort__popup">
+          <ul>
+            {sortList.map((sort, i) => (
+              <li
+                key={sort}
+                onClick={() => onClickSorted(i)}
+                className={selected === i ? 'active' : ''}>
+                {sort}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
