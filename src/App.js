0b05559901922757a8fe6b7ea523fe7ca9101f1f
MishaBelove96 <mishabelove72@gmail.com>
2023-05-19 16:50:02 +0400

    created db in mockapi and sent request to backend

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json';
 import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://6467661d2ea3cae8dc2d9f13.mockapi.io/items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+      });
+  }, []);
   return (
     <div className="wrapper">
       <Header />
@@ -18,7 +26,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((obj) => (
+            {items.map((obj) => (
               <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
