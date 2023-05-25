d85b51935cf6bd39b71330955f2a3ddb8b596756
MishaBelove96 <mishabelove72@gmail.com>
2023-05-25 15:32:30 +0400

    added logic to adding items to cart in home page

--- src/components/CardBlock/index.jsx
+++ src/components/CardBlock/index.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addItem } from '../../redux/slices/cartSlice';
 
-function CardBlock({ title, price, imageUrl, types, sizes }) {
+const typeNames = ['тонкое', 'традиционное'];
+
+function CardBlock({ id, title, price, imageUrl, types, sizes }) {
+  const dispatch = useDispatch();
+  const cartItem = useSelector((state) => state.cartReducer.items.find((obj) => obj.id === id));
   const [activeType, setActiveType] = React.useState(0);
   const [activeSize, setActiveSize] = React.useState(0);
-  const typeNames = ['тонкое', 'традиционное'];
+  const addedCount = cartItem ? cartItem.count : 0;
+
+  console.log(cartItem);
+
+  const onClickAdd = () => {
+    const item = {
+      id,
+      title,
+      price,
+      imageUrl,
+      type: typeNames[activeType],
+      size: activeSize,
+    };
+    dispatch(addItem(item));
+  };
   return (
     <div className="pizza-block">
       <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
@@ -32,7 +52,7 @@ function CardBlock({ title, price, imageUrl, types, sizes }) {
       </div>
       <div className="pizza-block__bottom">
         <div className="pizza-block__price">от {price} ₽</div>
-        <button className="button button--outline button--add">
+        <button onClick={onClickAdd} className="button button--outline button--add">
           <svg
             width="12"
             height="12"
@@ -45,7 +65,7 @@ function CardBlock({ title, price, imageUrl, types, sizes }) {
             />
           </svg>
           <span>Добавить</span>
-          <i>0</i>
+          {addedCount > 0 && <i>{addedCount}</i>}
         </button>
       </div>
     </div>
